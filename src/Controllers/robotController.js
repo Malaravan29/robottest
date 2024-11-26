6e03a5b708f5b1435f2c5c9307952671441d84b7
Your Name <malaravan.manimaran@logicalmindsit.com>
2024-11-26 15:48:25 +0530

    Robot

--- src/Controllers/robotController.js
+++ src/Controllers/robotController.js
@@ -3,12 +3,22 @@ import Robot from "../Models/Robot.js";
 // Controller to get robot 
 export const getRobotsByEmail = async (req, res) => {
   try {
-    const { email } = req.user; // Assuming email is extracted from the verified token
+    const { email, role } = req.user; // Assuming email and role are extracted from the verified token
 
-    const robots = await Robot.find({ emailId: email }).exec();
+    let robots;
 
-    if (!robots) {
-      return res.status(404).json({ message: "No robots found for this user" });
+    if (role === "user") {
+      // Fetch robots for a specific user based on their email
+      robots = await Robot.find({ emailId: email }).exec();
+    } else if (["Hr", "AdminController", "ProjectManage"].includes(role)) {
+      // Fetch all robots for privileged roles
+      robots = await Robot.find().exec();
+    } else {
+      return res.status(403).json({ message: "Access denied" });
+    }
+
+    if (!robots || robots.length === 0) {
+      return res.status(404).json({ message: "No robots found" });
     }
 
     return res.json(robots);
